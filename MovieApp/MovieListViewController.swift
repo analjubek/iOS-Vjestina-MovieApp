8598f8d3a3590ef12dceb10d6434167b703a8cf2
analjubek <ana.ljubek@gmail.com>
2022-04-05 21:10:46 +0200

    dz2 init

---
+++ MovieApp/MovieListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieListViewController.swift
+//  MovieApp
+//
+//  Created by Ana Ljubek on 05.04.2022..
+//
+
+import Foundation
+import UIKit
+import PureLayout
+import SnapKit
+import SwiftUI
+
+class MovieListViewController: UIViewController{
+    
+}
